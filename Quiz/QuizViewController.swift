7b9432f88117c5299d7b905abf82ca6290faff4f
Ryuma.h <ryuma810dragon@gmail.com>
2019-04-12 19:18:49 +0900

    Initial Commit

---
+++ Quiz/QuizViewController.swift
@@ -0,0 +1,78 @@
+//
+//  QuizViewController.swift
+//  Quiz
+//
+//  Created by litech on 2015/02/10.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    //クイズを格納する配列
+    var quizArray = [[Any]]()
+    
+    //正解数
+    var correctAnswer: Int = 0
+    
+    //クイズを表示するTextView
+    @IBOutlet var quizTextView: UITextView!
+    
+    //選択肢のボタン
+    @IBOutlet var choiceButtons1: UIButton!
+    @IBOutlet var choiceButtons2: UIButton!
+    @IBOutlet var choiceButtons3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var tmpArray = [[Any]]()
+        
+        //------------------------ここから下にクイズを書く------------------------//
+        tmpArray.append(["オバマ前大統領の身長は？", "140cm", "185cm", "192cm", 2])
+        tmpArray.append(["世界で２番目に国土の狭い国は？", "アメリカ合衆国", "オーストラリア", "モナコ公国", 3])
+        //------------------------ここから上にクイズを書く------------------------//
+        
+        quizArray = tmpArray.shuffled()
+
+        choiceQuiz()
+    }
+    
+    func choiceQuiz() {
+        quizTextView.text = quizArray[0][0] as? String
+        
+        //選択肢のボタンにそれぞれ選択肢のテキストをセット
+        choiceButtons1.setTitle(quizArray[0][1] as? String, for: .normal)
+        choiceButtons2.setTitle(quizArray[0][2] as? String, for: .normal)
+        choiceButtons3.setTitle(quizArray[0][3] as? String, for: .normal)
+    }
+    
+    @IBAction func choiceAnswer(sender: UIButton) {
+        if quizArray[0][4] as! Int == sender.tag {
+            //正解数を増やす
+            correctAnswer+=1
+        }
+        
+        quizArray.remove(at: 0)
+        //解いた問題数の合計が予め設定していた問題数に達したら結果画面へ
+        if quizArray.count == 0 {
+            performSegueToResult()
+        } else {
+            choiceQuiz()
+        }
+    }
+    
+    func performSegueToResult() {
+        performSegue(withIdentifier: "toResultView", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "toResultView") {
+            
+            let resultView = segue.destination as! ResultViewController
+            resultView.correctAnswer = self.correctAnswer
+        }
+    }
+}
+
+
